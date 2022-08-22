2c51af04a58eddc448143b629911510e7688d6f1
luis.pelozo <luis.pelozo@192.168.195.15>
2022-08-22 10:13:40 -0300

    Inicio do código

---
+++ construcaoCasa/src/visualizacao/EntradaSaida.java
@@ -0,0 +1,23 @@
+package visualizacao;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+public class EntradaSaida {
+	
+	public static int solicitaOpcao() {
+		
+		String[] opcoes = {"Consutrir casa" , "Movimentar portas ou Janelas", "Ver informações da casa", "Sair do programa"};
+		
+		JComboBox<String> menu = new JComboBox<String>(opcoes);
+		
+		JOptionPane.showConfirmDialog(null, menu, "Selecione a opção desejada", JOptionPane.OK_CANCEL_OPTION);
+		
+		return menu.getSelectedIndex();
+	}
+	
+	public static void exibeMsgEncerraPrograma() {
+		JOptionPane.showMessageDialog(null, "O programa será encerrado");
+	}
+
+}

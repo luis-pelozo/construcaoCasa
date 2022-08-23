676d1fe9e0bf7cfe88885f9c7ccb77584cafdceb
luis-pelozo <pelozoluis52@gmail.com>
2022-08-23 15:46:58 -0300

    Commit 2

--- construcaoCasa/src/Principal.java
+++ construcaoCasa/src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//teste de commit pelo vscodedd
+		//classe principal 
 		Controladora controladora = new Controladora();
 		controladora.exibeMenu();
 
